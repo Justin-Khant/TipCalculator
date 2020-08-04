88fc39390de0b9be9dabfd65451074b4b87249fd
Justin Khant <1kaungminkhant1@gmail.com>
2020-08-04 21:25:33 +0630

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Kaung Min Khant on 8/4/20.
+//  Copyright © 2020 Kaung Min Khant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
